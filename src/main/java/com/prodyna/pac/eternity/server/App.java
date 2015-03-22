7a2aa6503dc3328d77180ae64ebdc7930ed7e51f
Daniel Drachau <daniel@drachau.de>
2015-03-22 19:46:40 +0100

    initial commit

---
+++ src/main/java/com/prodyna/pac/eternity/server/App.java
@@ -0,0 +1,13 @@
+package com.prodyna.pac.eternity.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
